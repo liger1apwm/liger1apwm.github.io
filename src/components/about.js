344e482f19f696be3ae277b61a787be193c3dd75
Eric <ericaragundi@gmail.com>
2022-05-14 19:25:58 -0400

    routes created and some styles added

---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/pages.css'
+
+const About = () =>{
+  return (
+    <div>
+      <h3 className='heading'>This is the About page</h3>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+export default About;
---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/pages.css'
+
+const Home = () =>{
+  return (
+    <div>
+      <h3 className='heading'>This is the home page</h3>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+export default Home;
